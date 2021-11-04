c1bed7fae4e4a495f3ed56bc448c34931ce488fa
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-04 16:58:08 -0300

    refactor: move EmailValidator creatation to a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -7,13 +7,28 @@ interface SutTypes {
   emailValidatorStub: EmailValidator
 }
 
-const makeSut = (): SutTypes => {
+const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub {
     isValid (email: string): boolean {
       return true
     }
   }
-  const emailValidatorStub = new EmailValidatorStub()
+
+  return new EmailValidatorStub()
+}
+
+const makeEmailValidatorWitError = (): EmailValidator => {
+  class EmailValidatorStub {
+    isValid (email: string): boolean {
+      throw new Error()
+    }
+  }
+
+  return new EmailValidatorStub()
+}
+
+const makeSut = (): SutTypes => {
+  const emailValidatorStub = makeEmailValidator()
   const sut = new SignUpController(emailValidatorStub)
 
   return {
@@ -127,12 +142,7 @@ describe('SignUp Controller', () => {
   })
 
   test('Should return 500 if email validator throws', () => {
-    class EmailValidatorStub {
-      isValid (email: string): boolean {
-        throw new Error('Error')
-      }
-    }
-    const emailValidatorStub = new EmailValidatorStub()
+    const emailValidatorStub = makeEmailValidatorWitError()
     const sut = new SignUpController(emailValidatorStub)
 
     const httpRequest = {
