6b249e906b88e6308db8fe22881f6985e361478f
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-04 13:37:37 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -16,4 +16,20 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Raymond Guerrero',
+        password: 'hy6YJ4o5Ep65fLpSUjY',
+        passwordConfirmation: 'hy6YJ4o5Ep65fLpSUjY'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
