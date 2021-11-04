d5ecff8bc51369e73a42f023b57d90c42bb57d2b
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-04 18:11:12 -0300

    feat: ensure SignUpController return user if valid data is passed

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -30,11 +30,11 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.add({ email, name, password })
+      const account = this.addAccount.add({ email, name, password })
 
       return {
-        statusCode: 200,
-        body: {}
+        statusCode: 201,
+        body: account
       }
     } catch (error) {
       return serverError()
