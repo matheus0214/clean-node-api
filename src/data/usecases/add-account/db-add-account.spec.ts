6a6a9205daa59a71794e8ae7f937b74ebdc16a17
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-06 17:33:24 -0300

    test:  ensure DbAddAccount calss AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,9 +1,10 @@
-import { Encrypter } from './db-add-account-protocols'
+import { AccountModel, AddAccountModel, Encrypter, AddAccountRepository } from './db-add-account-protocols'
 import { DbAddAccount } from './db-add-account'
 
 interface DbAccountTypes {
   sut: DbAddAccount
   encrypterStub: Encrypter
+  addAccountRepositoryStub: AddAccountRepository
 }
 
 const makeEncrypter = (): Encrypter => {
@@ -16,12 +17,27 @@ const makeEncrypter = (): Encrypter => {
   return new EncrypterStub()
 }
 
+const makeAddAccountRepositoryStub = (): AddAccountRepository => {
+  class AddAccountRepositoryStub implements AddAccountRepository {
+    async add (account: AddAccountModel): Promise<AccountModel> {
+      return await new Promise(resolve => resolve({
+        id: 'valid_id',
+        ...account
+      }))
+    }
+  }
+
+  return new AddAccountRepositoryStub()
+}
+
 const makeSut = (): DbAccountTypes => {
   const encrypterStub = makeEncrypter()
+  const addAccountRepositoryStub = makeAddAccountRepositoryStub()
 
   return {
-    sut: new DbAddAccount(encrypterStub),
-    encrypterStub
+    sut: new DbAddAccount(encrypterStub, addAccountRepositoryStub),
+    encrypterStub,
+    addAccountRepositoryStub
   }
 }
 
@@ -58,5 +74,25 @@ describe('DbAddAccount UseCase', () => {
     const promise = sut.add(accountData)
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('Should call AddAccountRepository with correct values', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+
+    const accountData = {
+      name: 'Helen Snyder',
+      email: 'ewe@elakono.pl',
+      password: '1234'
+    }
+
+    const addSpy = jest.spyOn(addAccountRepositoryStub, 'add')
+
+    await sut.add(accountData)
+
+    expect(addSpy).toHaveBeenCalledWith({
+      name: 'Helen Snyder',
+      email: 'ewe@elakono.pl',
+      password: 'value_encrypt'
+    })
   })
 })
