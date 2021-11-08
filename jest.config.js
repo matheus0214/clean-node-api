20b5d007ce5bf5aeac773b3cbdedc1588b0c1c22
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-08 14:09:48 -0300

    test: refactor get correct status when signup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverage: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts',
+  '!<rootDir>/src/main/*'],
   coverageDirectory: 'coverage',
   coverageProvider: 'node',
   transform: {
