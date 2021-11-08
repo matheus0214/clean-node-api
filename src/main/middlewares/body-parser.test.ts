d306616b31f77876aa77bce2842744c7d8e4976a
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-08 10:31:53 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,18 @@
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('Body parser middlware', () => {
+  test('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({
+        name: 'Rodrigo'
+      })
+      .expect({ name: 'Rodrigo' })
+  })
+})
