e2334532606422f62effb29eb9248a6bdaf75bf1
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-12 14:16:00 -0300

    test: add test to CompareFieldValidation

---
+++ src/presentation/helpers/validators/compare-field-validation.spec.ts
@@ -0,0 +1,28 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-field-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('password', 'confirmation')
+}
+
+describe('CompareFields Validation', () => {
+  test('Should return a MissingParamError if validation fails', () => {
+    const sut = makeSut()
+    const error = sut.validate({
+      password: 'any',
+      confirmation: 'wrong'
+    })
+
+    expect(error).toEqual(new InvalidParamError('confirmation'))
+  })
+
+  test('Should not return a if validation suceeds', () => {
+    const sut = makeSut()
+    const success = sut.validate({
+      password: 'any',
+      confirmation: 'any'
+    })
+
+    expect(success).toBeFalsy()
+  })
+})
