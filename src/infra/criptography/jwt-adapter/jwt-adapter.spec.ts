a9c065ead5a809026f54088de648c079fd3f4c37
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-17 22:01:09 -0300

    test: ensure JwtAdapter returns a correct token

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -2,6 +2,12 @@ import jwt from 'jsonwebtoken'
 
 import { JwtAdapter } from './jwt-adapter'
 
+jest.mock('jsonwebtoken', () => ({
+  sign (): string {
+    return 'access_token'
+  }
+}))
+
 describe('JWT Adapter', () => {
   test('Should call sign with correct values', async () => {
     const sut = new JwtAdapter('secret')
@@ -11,5 +17,13 @@ describe('JWT Adapter', () => {
     await sut.encrypt('any_id')
 
     expect(spySign).toBeCalledWith({ id: 'any_id' }, 'secret')
+  })
+
+  test('Should return a token on sign success', async () => {
+    const sut = new JwtAdapter('secret')
+
+    const token = await sut.encrypt('any_id')
+
+    expect(token).toBe('access_token')
   })
 })
