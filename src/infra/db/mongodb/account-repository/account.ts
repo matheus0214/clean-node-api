bbb98dd18914c82ea586b556ec93862384d6fd04
Matheus Gomes <matheusgiga123@gmail.com>
2021-11-21 13:45:53 -0300

    feat: ensure AccountMongoRepository returns on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models'
 import { AddAccountModel } from '../../../../domain/usecases'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (account: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
 
@@ -12,5 +13,12 @@ export class AccountMongoRepository implements AddAccountRepository {
     const newAccount = await accountCollection.findOne(insertedId)
 
     return MongoHelper.map(newAccount)
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel | null> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+
+    return account ? MongoHelper.map(account) : null
   }
 }
