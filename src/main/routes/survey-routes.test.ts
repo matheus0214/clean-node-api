576521ccc778520a33371d77a10920969ab6bc04
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-01-05 15:50:41 -0300

    test: ensure load surveys route returns 200 if a valid token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -80,5 +80,50 @@ describe('SurveyRoutes', () => {
         .get('/api/surveys')
         .expect(403)
     })
+
+    test('Should return 200 on load surveys with valid token', async () => {
+      const user = await accountCollection.insertOne({
+        email: 'fulfiwbur@vubo.af',
+        password: 'any',
+        name: 'Viola Park',
+        role: 'user'
+      })
+
+      const id = String(user.insertedId)
+
+      const accessToken = sign({ id }, env.jwtSecret)
+
+      await accountCollection.updateOne({
+        _id: new ObjectId(id)
+      }, {
+        $set: {
+          accessToken
+        }
+      })
+
+      await surveyCollection.insertMany([
+        {
+          question: 'any_question',
+          answers: [{
+            answer: 'any_answer',
+            image: 'any_image'
+          }],
+          date: new Date()
+        },
+        {
+          question: 'other_question',
+          answers: [{
+            answer: 'other_answer',
+            image: 'other_image'
+          }],
+          date: new Date()
+        }
+      ])
+
+      await request(app)
+        .get('/api/surveys')
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
