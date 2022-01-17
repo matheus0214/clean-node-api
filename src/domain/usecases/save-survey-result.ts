9e22855fec004b1a37fc88a53f3896f16ba4cb01
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-01-17 20:04:00 -0300

    feat: add SaveSurveyResult usecase

---
+++ src/domain/usecases/save-survey-result.ts
@@ -0,0 +1,7 @@
+import { SurveyResultModel } from '../models/survey-result'
+
+export type SaveSurveyResultModel = Omit<SurveyResultModel, 'id'>
+
+export type SaveSurveyResult = {
+  save: (survey: SaveSurveyResultModel) => Promise<SurveyResultModel>
+}
