1c577e98a5aa0e1aa3129d09690c276e10fa3da1
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-01-19 16:52:42 -0300

    feat: ensure SaveSurveyResultController returns 403 if surveyId is not register

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,3 +1,5 @@
+import { InvalidParamError } from '../../login/login/login-controller-protocols'
+import { forbidden } from '../../login/signup/signup-controller-protocols'
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from './save-survey-result-controller-protocols'
 
 export class SaveSurveyResultController implements Controller {
@@ -6,7 +8,11 @@ export class SaveSurveyResultController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadSurveyById.loadById(httpRequest.params.survey_id)
+    const survey = await this.loadSurveyById.loadById(httpRequest.params.survey_id)
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
+
     return {
       body: {},
       statusCode: 200
