e33a7c2636f83cf581b539925f35b7a552764aa2
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-01-19 15:13:50 -0300

    feat: ensure SurveyResultMongoRepository insert new survey result if there is no data register

--- src/data/protocols/db/survey/save-survey-result-repository.ts
+++ src/data/protocols/db/survey/save-survey-result-repository.ts
@@ -2,5 +2,5 @@ import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result'
 
 export interface SaveSurveyResultRepository {
-  save: (date: SaveSurveyResultModel) => Promise<SurveyResultModel | undefined>
+  save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel | undefined>
 }
---
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -0,0 +1,28 @@
+import {
+  SaveSurveyResultModel,
+  SaveSurveyResultRepository,
+  SurveyResultModel
+} from '@/data/usecases/save-survey-result/db-save-survey-result-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyResultMongoRepository implements
+    SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel | undefined> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+
+    const result = await surveyResultCollection.findOneAndUpdate({
+      surveyId: data.surveyId,
+      accountId: data.accountId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnDocument: 'after'
+    })
+
+    return result.value ? MongoHelper.map(result.value) : undefined
+  }
+}
