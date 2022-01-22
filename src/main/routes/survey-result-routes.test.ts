20c2f0cbf5aa0ec3fe0382ea2831710b87e55797
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-01-22 16:27:15 -0300

    test: ensure save survey result returns 403 if no token is provided

---
+++ src/main/routes/survey-result-routes.test.ts
@@ -0,0 +1,16 @@
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('SurveyRoutes', () => {
+  describe('PUT /surveys/:surveyId/results', () => {
+    test('Should return 403 on save survey result without accessToken', async () => {
+      await request(app)
+        .put('/api/surveys/any_id/results')
+        .send({
+          answer: 'any_answer'
+        })
+        .expect(403)
+    })
+  })
+})
