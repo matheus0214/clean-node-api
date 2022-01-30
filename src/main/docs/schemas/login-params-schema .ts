bb755f099b695c50a84ebcb56dd04b97b7471883
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-01-30 14:26:02 -0300

    docs: add login api docs

---
+++ src/main/docs/schemas/login-params-schema .ts
@@ -0,0 +1,12 @@
+export const loginParamsSchema = {
+  type: 'object',
+  required: ['email', 'password'],
+  properties: {
+    email: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    }
+  }
+}
