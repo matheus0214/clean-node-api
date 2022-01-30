1529715f0a65a832507e9ab4ffcfa2e3cbc45969
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-01-30 13:35:48 -0300

    test: add no cache middleware

---
+++ src/main/middlewares/no-cache.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest'
+
+import app from '../config/app'
+import { noCache } from '../middlewares'
+
+describe('NoCache middleware', () => {
+  test('Should disable cache', async () => {
+    app.get('/test_cache', noCache, (req, res) => {
+      res.send()
+    })
+
+    await request(app)
+      .get('/test_cache')
+      .expect('cache-control', 'no-store, no-cache, must-revalidate, proxy-revalidate')
+      .expect('pragma', 'no-cache')
+      .expect('expires', '0')
+      .expect('surrogate-control', 'no-store')
+  })
+})
