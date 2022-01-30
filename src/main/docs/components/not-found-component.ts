715558da1a64283ecc02af0b935517b7527df864
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-01-30 14:41:39 -0300

    docs: update login api docs

---
+++ src/main/docs/components/not-found-component.ts
@@ -0,0 +1,10 @@
+export const notFoundComponent = {
+  description: 'Not Found',
+  content: {
+    'application/json': {
+      schema: {
+        $ref: '#/schemas/error'
+      }
+    }
+  }
+}
--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,5 +1,10 @@
+import { badRequestComponent } from './components/bad-request-component'
+import { notFoundComponent } from './components/not-found-component'
+import { serverErrorComponent } from './components/server-error-component'
+import { unauthorizedComponent } from './components/unauthorized-component'
 import { loginPath } from './paths/login-path'
 import { accountSchema } from './schemas/account-schema'
+import { errorSchema } from './schemas/error-schema'
 import { loginParamsSchema } from './schemas/login-params-schema '
 
 export default {
@@ -8,6 +13,10 @@ export default {
     title: 'Clean Node API',
     description: 'API do curso do Mango',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL',
+    url: ''
   },
   servers: [
     { url: '/api' }
@@ -20,6 +29,13 @@ export default {
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest: badRequestComponent,
+    serverError: serverErrorComponent,
+    unauthorized: unauthorizedComponent,
+    notFound: notFoundComponent
   }
 }
