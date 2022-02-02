1a2deaf0918703b8a34790d8a0c4c7131486c8e7
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-02-01 22:32:04 -0300

    refactor: move imports to a single protocol file

---
+++ src/data/usecases/load-survey-result/db-load-survey-result-protocols.ts
@@ -0,0 +1,3 @@
+export { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
+export { LoadSurveyResult } from '@/domain/usecases/survey-result/load-survey-result'
+export { SurveyResultModel } from '../survey-result/save-survey-result/db-save-survey-result-protocols'
