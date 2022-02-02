b8fa570f1718ea40c34373b1b56a77b4f2b2393c
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-02-02 10:43:49 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.spec.ts
@@ -1,6 +1,6 @@
 import { mockLoadSurveyById } from '@/presentation/test/mock-load-survey'
 import { LoadSurveyResultController } from './load-survey-result-controller'
-import { HttpRequest } from './load-survey-result-controller-protocols'
+import { HttpRequest, LoadSurveyById } from './load-survey-result-controller-protocols'
 
 const mockRequest = (): HttpRequest => {
   return {
@@ -10,12 +10,25 @@ const mockRequest = (): HttpRequest => {
   }
 }
 
+type SutTypes = {
+  sut: LoadSurveyResultController
+  loadSurveyByIdStub: LoadSurveyById
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyByIdStub = mockLoadSurveyById()
+
+  const sut = new LoadSurveyResultController(loadSurveyByIdStub)
+
+  return {
+    sut,
+    loadSurveyByIdStub
+  }
+}
+
 describe('LoadSurveyResultController', () => {
   test('should call LoadSurveyById with correct value', async () => {
-    const loadSurveyByIdStub = mockLoadSurveyById()
-
-    const sut = new LoadSurveyResultController(loadSurveyByIdStub)
-
+    const { loadSurveyByIdStub, sut } = makeSut()
     const spy = jest.spyOn(loadSurveyByIdStub, 'loadById')
 
     await sut.handle(mockRequest())
