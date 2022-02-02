256c7d8138b5f3a067b7b701277643d7a70a1bbd
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-02-02 10:38:09 -0300

    feat: ensure LoadSurveyResultController call LoadSurveyById with correct id

---
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller-protocols.ts
@@ -0,0 +1,6 @@
+export {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from '../save-survey-result/save-survey-result-controller-protocols'
+export { LoadSurveyById } from '../save-survey-result/save-survey-result-controller-protocols'
