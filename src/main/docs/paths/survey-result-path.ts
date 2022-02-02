e7c963f93bec495d9db253de23543866dc735247
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-02-02 13:08:55 -0300

    docs: add load survey result api docs

--- src/main/docs/paths/survey-result-path.ts
+++ src/main/docs/paths/survey-result-path.ts
@@ -42,5 +42,39 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    tags: ['Survey'],
+    summary: 'API para carregar resposta de uma enquente',
+    security: [{ apiKeyAuth: [] }],
+    parameters: {
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    },
+    responses: {
+      200: {
+        description: 'Successful',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
