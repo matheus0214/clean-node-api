f235dd68b6d9add051e223aa918ccef9bc0e4e94
Matheus Gomes Dias <matheusgiga123@gmail.com>
2022-02-01 22:40:28 -0300

    test: ensure DbLoadSurveyResult returns a survey result on success

--- src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -1,5 +1,8 @@
+import MockDate from 'mockdate'
+
 import { LoadSurveyResultRepository } from '@/data/protocols/db/survey-result/load-survey-result-repository'
 import { mockLoadSurveyResultRepository } from '@/data/test'
+import { mockSurveyResultModel } from '@/domain/test/mock-survey-result'
 import { DbLoadSurveyResult } from './db-load-survey-result'
 
 type SutTypes = {
@@ -15,6 +18,14 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbLoadSurveyResult Usecase', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+
+  afterAll(() => {
+    MockDate.reset()
+  })
+
   test('should call LoadSurveyResultRepository', async () => {
     const { sut, loadSurveyResultRepositoryStub } = makeSut()
 
@@ -35,5 +46,13 @@ describe('DbLoadSurveyResult Usecase', () => {
     const promise = sut.load('any_survey_id')
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test('should return surveyResultModel on success', async () => {
+    const { sut } = makeSut()
+
+    const response = await sut.load('any_survey_id')
+
+    expect(response).toEqual(mockSurveyResultModel())
   })
 })
